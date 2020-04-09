705bb2537f98da44c018c7a909e05cc5963bf38d
Santosh Casula <31424786+SantoshCasula@users.noreply.github.com>
2020-04-08 23:04:53 -0400

    Creating Data tables for all the products in the product line

---
+++ force-app/main/default/lwc/overdriveComponent/fetchDataHelper.js
@@ -0,0 +1,19 @@
+const recordMetadata = {
+  name: "Family"
+};
+
+export default function fetchDataHelper({ amountOfRecords }) {
+  return fetch(
+    "https://rossvideo--qa.lightning.force.com/lightning/r/a8nP00000008QzkIAE/related/Products__r/view",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify({
+        amountOfRecords,
+        recordMetadata
+      })
+    }
+  ).then(response => response.json());
+}
--- force-app/main/default/lwc/overdriveComponent/overdriveComponent.js
+++ force-app/main/default/lwc/overdriveComponent/overdriveComponent.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
+import fetchDataHelper from "./fetchDataHelper";
 
-export default class OverdriveComponent extends LightningElement {}
+const columns = [{ label: "Product Family", fieldName: "Family" }];
+
+export default class overdriveComponent extends LightningElement {
+  @track data = [];
+  @track columns = columns;
+
+  // eslint-disable-next-line @lwc/lwc/no-async-await
+  async connectedCallback() {
+    const data = await fetchDataHelper({ amountOfRecords: 100 });
+    this.data = data;
+  }
+}
